66d989f10c3281b724b82e16c7b9964ef8f37865
stingliang <liangrui5526@126.com>
2021-09-29 18:03:41 +0800

    add common.h for including conveniently

---
+++ common.h
@@ -0,0 +1,21 @@
+//
+// Created by Liang on 2021/9/29.
+//
+
+#ifndef DEV_UTILITY_LIBRARY_COMMON_H
+#define DEV_UTILITY_LIBRARY_COMMON_H
+
+// std include
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <bitset>
+#include <deque>
+#include <thread>
+#include <mutex>
+#include <chrono>
+#include <map>
+#include <set>
+
+#endif //DEV_UTILITY_LIBRARY_COMMON_H
